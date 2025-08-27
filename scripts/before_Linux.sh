5498411c60ea3901ecc25ee4dc96076ccfa83c57
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-08-27 16:25:11 +0900

    Add tests and disable static linking

--- scripts/before_Linux.sh
+++ scripts/before_Linux.sh
@@ -6,11 +6,11 @@ set -eux
 if command -v apk &> /dev/null; then
     apk add --no-cache openblas-dev swig ccache
 elif command -v dnf &> /dev/null; then
-    dnf install -y openblas-devel openblas-static swig ccache
+    dnf install -y openblas-devel swig ccache
 elif command -v apt &> /dev/null; then
     apt install -y libopenblas-dev swig ccache
 elif command -v yum &> /dev/null; then
-    yum install -y openblas-devel openblas-static swig ccache
+    yum install -y openblas-devel swig ccache
 else
     echo "Unsupported package manager. Please install dependencies manually."
     exit 1
