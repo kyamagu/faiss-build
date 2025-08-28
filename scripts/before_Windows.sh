34acf346bf76f77a437baa1bcb7c95b0011b7e85
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-08-28 09:32:05 +0900

    Add before script

---
+++ scripts/before_Windows.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# Function to install OpenBLAS for Windows from https://github.com/OpenMathLib/OpenBLAS/releases
+function install_openblas() {
+    echo "Installing OpenBLAS for Windows..."
+    local OPENBLAS_VERSION="0.3.30"
+    local OPENBLAS_URL="https://github.com/OpenMathLib/OpenBLAS/releases/download/v${OPENBLAS_VERSION}/OpenBLAS-${OPENBLAS_VERSION}-$1.zip"
+    local ZIP_PATH="$RUNNER_TEMP/OpenBLAS.zip"
+    local DEST_PATH=${ProgramFiles}/OpenBLAS
+    curl -sL "$OPENBLAS_URL" -o "$ZIP_PATH"
+    mkdir -p "$DEST_PATH"
+
+    # Extract to destination
+    powershell.exe -Command "Expand-Archive -Path '$ZIP_PATH' -DestinationPath '$DEST_PATH' -Force"
+    powershell.exe -Command "Move-Item $DEST_PATH/OpenBLAS*/* $DEST_PATH/ -Force; Remove-Item $DEST_PATH/OpenBLAS* -Recurse"
+}
+
+# Install system dependencies
+if [[ "$PROCESSOR_IDENTIFIER" == ARM* ]]; then
+    # NOTE: PROCESSOR_ARCHITECTURE is incorrectly set to "AMD64" on emulated ARM64 Windows runners.
+    install_openblas woa64-dll
+else
+    install_openblas x64
+fi
