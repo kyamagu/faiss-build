5000ebe354fa5fbe06ab0dd73f208ce83a9beeb8
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-08-30 16:45:55 +0900

    Use Stable ABI (#3)

--- scripts/before_Linux.sh
+++ scripts/before_Linux.sh
@@ -4,13 +4,13 @@ set -eux
 
 # Install system dependencies
 if command -v apk &> /dev/null; then
-    apk add --no-cache openblas-dev swig ccache
+    apk add --no-cache openblas-dev ccache
 elif command -v dnf &> /dev/null; then
-    dnf install -y openblas-devel swig ccache
+    dnf install -y openblas-devel ccache
 elif command -v apt &> /dev/null; then
-    apt install -y libopenblas-dev swig ccache
+    apt install -y libopenblas-dev ccache
 elif command -v yum &> /dev/null; then
-    yum install -y openblas-devel swig ccache
+    yum install -y openblas-devel ccache
 else
     echo "Unsupported package manager. Please install dependencies manually."
     exit 1
