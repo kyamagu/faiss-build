35e7041ffaae38822ec38263d76be5be181c962b
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-09-02 07:39:39 +0900

    Fix inconsistent MACOSX_DEPLOYMENT_TARGET

--- scripts/install_macOS.sh
+++ scripts/install_macOS.sh
@@ -8,7 +8,7 @@ brew install libomp
 # Set MACOSX_DEPLOYMENT_TARGET.
 MACOS_VERSION=$(sw_vers -productVersion)
 if [[ "$MACOS_VERSION" =~ ^13\. ]]; then
-    echo "MACOSX_DEPLOYMENT_TARGET=13.0" >> $GITHUB_ENV
+    echo "MACOSX_DEPLOYMENT_TARGET=10.13" >> $GITHUB_ENV
 else
-    echo "MACOSX_DEPLOYMENT_TARGET=14.0" >> $GITHUB_ENV
+    echo "MACOSX_DEPLOYMENT_TARGET=10.14" >> $GITHUB_ENV
 fi
