0c56d41127a5e2d02891be2892e00e424a852b02
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-09-01 23:49:59 +0900

    Override MACOSX_DEPLOYMENT_TARGET in CI

--- scripts/install_macOS.sh
+++ scripts/install_macOS.sh
@@ -4,3 +4,11 @@ set -eux
 
 # Install system dependencies.
 brew install libomp
+
+# Set MACOSX_DEPLOYMENT_TARGET.
+MACOS_VERSION=$(sw_vers -productVersion)
+if [ "$MACOS_VERSION" == 13* ]; then
+    echo "MACOSX_DEPLOYMENT_TARGET=13.0" >> $GITHUB_ENV
+else
+    echo "MACOSX_DEPLOYMENT_TARGET=14.0" >> $GITHUB_ENV
+fi
