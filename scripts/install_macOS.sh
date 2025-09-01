b32b3f7533d73079898780e4a60f2d516c56d831
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-09-02 00:11:29 +0900

    Add whitespace removal

--- scripts/install_macOS.sh
+++ scripts/install_macOS.sh
@@ -6,7 +6,7 @@ set -eux
 brew install libomp
 
 # Set MACOSX_DEPLOYMENT_TARGET.
-MACOS_VERSION=$(sw_vers -productVersion)
+MACOS_VERSION=$(sw_vers -productVersion | xargs)
 if [ "$MACOS_VERSION" == 13* ]; then
     echo "MACOSX_DEPLOYMENT_TARGET=13.0" >> $GITHUB_ENV
 else
