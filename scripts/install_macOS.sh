dd717278b06bec03bbbc70e219b5e77dc109df54
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-09-01 22:59:39 +0900

    Split build steps

---
+++ scripts/install_macOS.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# Install system dependencies.
+brew install libomp
+
+# Set MACOSX_DEPLOYMENT_TARGET based on the current OS version.
+MACOS_VERSION=$(sw_vers -productVersion)
+if [[ "$MACOS_VERSION" == 13* ]]; then
+    echo "MACOSX_DEPLOYMENT_TARGET=13.0" >> $GITHUB_ENV
+else
+    echo "MACOSX_DEPLOYMENT_TARGET=14.0" >> $GITHUB_ENV
+fi
