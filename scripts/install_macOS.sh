d71d0095840c8b4dce2768aa805adc14a006cc19
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-09-02 00:16:09 +0900

    Fix macOS version detection

--- scripts/install_macOS.sh
+++ scripts/install_macOS.sh
@@ -3,11 +3,11 @@
 set -eux
 
 # Install system dependencies.
-brew install libomp
+# brew install libomp
 
 # Set MACOSX_DEPLOYMENT_TARGET.
-MACOS_VERSION=$(sw_vers -productVersion | xargs)
-if [ "$MACOS_VERSION" == 13* ]; then
+MACOS_VERSION=$(sw_vers -productVersion)
+if [[ "$MACOS_VERSION" =~ ^13\. ]]; then
     echo "MACOSX_DEPLOYMENT_TARGET=13.0" >> $GITHUB_ENV
 else
     echo "MACOSX_DEPLOYMENT_TARGET=14.0" >> $GITHUB_ENV
