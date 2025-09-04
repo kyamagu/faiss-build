aa9985d3f605a598b5d992a9a49a16cda73f59bd
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-09-04 22:46:38 +0900

    Add ROCm build job

--- scripts/install_Linux.sh
+++ scripts/install_Linux.sh
@@ -2,6 +2,8 @@
 
 set -eux
 
+FAISS_GPU_SUPPORT=${FAISS_GPU_SUPPORT:-OFF}
+
 # OpenBLAS installation
 function install_openblas() {
     if command -v apk &> /dev/null; then
@@ -19,16 +21,15 @@ function install_openblas() {
 
 # CUDA installation
 function install_cuda() {
-    ARCH=$(uname -m)
-    CUDA_VERSION=${CUDA_VERSION:-12.8}
-    CUDA_PACKAGE_VERSION=${CUDA_VERSION//./-}
+    local ARCH=$(uname -m)
+    local CUDA_VERSION=${CUDA_VERSION:-12.8}
+    local CUDA_PACKAGE_VERSION=${CUDA_VERSION//./-}
     if command -v apk &> /dev/null; then
-        DISTRO=${DISTRO:-alpine}
         echo "CUDA installation on Alpine is not supported yet."
         exit 1
     elif command -v dnf &> /dev/null; then
         # TODO: Detect DISTRO via /etc/*-release.
-        DISTRO=${DISTRO:-rhel8}
+        local DISTRO=${DISTRO:-rhel8}
         dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/${DISTRO}/${ARCH}/cuda-${DISTRO}.repo
         dnf install -y \
             cuda-nvcc-${CUDA_PACKAGE_VERSION} \
@@ -37,7 +38,7 @@ function install_cuda() {
             libcublas-devel-${CUDA_PACKAGE_VERSION} \
             libcurand-devel-${CUDA_PACKAGE_VERSION}
     elif command -v apt &> /dev/null; then
-        DISTRO=${DISTRO:-ubuntu2404}
+        local DISTRO=${DISTRO:-ubuntu2404}
         wget https://developer.download.nvidia.com/compute/cuda/repos/${DISTRO}/${ARCH}/cuda-keyring_1.1-1_all.deb
         dpkg -i cuda-keyring_1.1-1_all.deb
         apt update && apt install -y \
@@ -51,7 +52,45 @@ function install_cuda() {
     fi
 }
 
+# ROCm installation
+function install_rocm() {
+    local ROCM_VERSION=${ROCM_VERSION:-6.4.3}
+    if command -v apk &> /dev/null; then
+        echo "ROCm installation on Alpine is not supported yet."
+        exit 1
+    elif command -v dnf &> /dev/null; then
+        local DISTRO=${DISTRO:-el8}
+        tee /etc/yum.repos.d/rocm.repo <<EOF
+[ROCm-${ROCM_VERSION}]
+name=ROCm${ROCM_VERSION}
+baseurl=https://repo.radeon.com/rocm/${DISTRO}/${ROCM_VERSION}/main
+enabled=1
+priority=50
+gpgcheck=1
+gpgkey=https://repo.radeon.com/rocm/rocm.gpg.key
+EOF
+        dnf clean all
+        dnf install -y \
+            rocm-developer-tools \
+            rocm-hip-runtime-devel
+    elif command -v apt &> /dev/null; then
+        local DISTRO=${DISTRO:-noble}
+        wget https://repo.radeon.com/rocm/rocm.gpg.key -O - \
+            | gpg --dearmor | sudo tee /etc/apt/keyrings/rocm.gpg > /dev/null
+        echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/${ROCM_VERSION} ${DISTRO} main" \
+            | tee /etc/apt/sources.list.d/rocm.list
+        echo -e 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' \
+            | tee /etc/apt/preferences.d/rocm-pin-600
+        apt update
+        apt install -y rocm-hip-runtime-devel
+    else
+        echo "Unsupported package manager. Please install ROCm manually."
+    fi
+}
+
 install_openblas
-if [ ! "${FAISS_GPU_SUPPORT:-OFF}" = "OFF" ]; then
+if [ "$FAISS_GPU_SUPPORT" = "CUDA" ] || [ "$FAISS_GPU_SUPPORT" = "CUVS" ]; then
     install_cuda
+elif [ "$FAISS_GPU_SUPPORT" = "ROCM" ]; then
+    install_rocm
 fi
