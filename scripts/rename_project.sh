90b19aa731b6fdc5b43b95f5a9fa98339ef8b13b
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-09-09 12:38:57 +0900

    Add OS check

--- scripts/rename_project.sh
+++ scripts/rename_project.sh
@@ -2,4 +2,10 @@
 
 set -eux
 
-sed -i '' "s/^name = \"faiss-cpu\"/name = \"${1}\"/" pyproject.toml
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS/BSD sed requires an argument for -i (can be empty)
+  sed -i '' "s/^name = \"faiss-cpu\"/name = \"${1}\"/" pyproject.toml
+else
+  # GNU sed (Linux) works with just -i
+  sed -i "s/^name = \"faiss-cpu\"/name = \"${1}\"/" pyproject.toml
+fi
